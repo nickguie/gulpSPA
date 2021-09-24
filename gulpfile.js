47eeac17cad0c8effdd20753841d260344a13e4e
Nickolas Guilherme Pereira de Almeida <niguiealmeida@hotmail.com>
2021-09-23 21:06:30 -0300

    gulp simple SPA

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const { series, parallel} = require('gulp')
 const gulp = require('gulp')
 
+
 const { appHTML, appCSS, appJS, appIMG } = require('./gulpTasks/app')
 const { depsCSS, depsFonts } = require('./gulpTasks/deps')
 const { monitorarArquivos, servidor } = require('./gulpTasks/servidor')
